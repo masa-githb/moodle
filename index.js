4a7b2c80571381b1f3f4b9d027a87b8012da61a4
masa-githb <tsurunosono@gmail.com>
2025-09-03 15:31:46 +0900

    Update index.js

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ app.get("/", (req, res) => {
   res.send("Hello from Render + GitHub!");
 });
 
-app.listen(3000, () => {
-  console.log("Server running on port 3000");
+// Render が割り当てるポート番号を使う
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
 });
